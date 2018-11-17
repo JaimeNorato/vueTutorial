9a9637914ae7b436cc8455e0aeda68dff1c55836
Jaime Norato <jn901113jn@gmail.com>
2018-11-16 23:44:38 -0500

    propiedades computadas

---
+++ js/computed_properties.js
@@ -0,0 +1,25 @@
+new Vue({
+    el:'#main',
+    data:{
+        nombre:'',
+        edad:'',
+        sexo:'',
+        correoe:''
+    },
+
+    methods:{
+        enviarDatos:function () {
+            if(this.nombre && this.edad && this.sexo && this.correoe){
+                alert("Enviando datos....");
+            }else {
+                alert("Rellene todos los campos.");
+            }
+        }
+    },
+
+    computed:{
+        todaLaInformacion: function () {
+            return this.nombre && this.edad && this.sexo && this.correoe;
+        }
+    }
+});
